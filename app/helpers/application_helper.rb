24c31c9c180e3c0e7fc051d00199fc6f1c6402dd
LesleyMdCampo <lesliz32@gmail.com>
2014-01-07 10:33:58 -0600

    Add visitor helper method for logged_out users.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,19 @@ module ApplicationHelper
     session[:user]
   end
 
+  def visitor
+  	session[:logged_out]
+  end
+
 # Displays wrapped content if a user is logged in.
 #
 # Returns nothing.
 	def user_content(&block)
   	capture(&block) if current_user
+	end
+
+	def non_user(&block)
+		capture(&block) if visitor
 	end
 
 end
